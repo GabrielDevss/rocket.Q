6b40e1c53d4707f6c7fcc74bca64d3b0b55da362
GabrielDevss <gabriel0211oliveira15@gmail.com>
2022-03-23 11:13:54 -0300

    rocketQ

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const QuestionControllers = require('./controllers/QuestionControllers');
+const RoomControllers = require('./controllers/RoomControllers');
+
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => res.render('index', { page: 'enter-room' }));
+routes.get('/create-pass', (req, res) => res.render('index', { page: 'create-pass' }));
+
+routes.post('/create-room', RoomControllers.create);  
+routes.get('/room/:room', RoomControllers.open);
+
+routes.post('/question/create/:room', QuestionControllers.create);
+routes.post('/question/:room/:question/:action', QuestionControllers.index);
+
+module.exports = routes;
